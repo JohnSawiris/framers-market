fb4a970cd4e933e25a39c19bd3fcd4e8bb7cd37d
Cheryl Frankenfield and John Sawiris <cheryl@>
2018-02-06 16:25:27 -0800

    add routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import Header from './Header';
 import Week from './Week';
 import Months from './Months';
+import { Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return(
     <div>
       <Header />
-      <Week />
-      <hr />
-      <Months />
+      <Switch>
+         <Route exact path='/' component={Months} />
+         <Route path='/events' component={Week} />
+        <Week />
+        <hr />
+        <Months />
+      </Switch>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
+
   return(
     <nav className="center">
-      <style jsx>
+      <style global jsx>
         {`
-          .center {
-            margin: 6rem auto 3rem;
-            width: 80%;
-            text-align: center;
-          }
-          a {
+         .anchor, a {
             margin: .5rem;
             padding: 1rem;
             color: #5f3812;
             transition:  all .4s ease-in-out;
           }
-          a:hover {
+          .anchor:hover, a:hover {
             text-decoration: none;
             background-color: #5f3812;
             color: #fff;
           }
           `}
       </style>
-      <a href="#">Markets</a>
-      <a href="#">Vendors</a>
-      <a href="#">Community</a>
+      <style  jsx>
+        {`
+          .center {
+            margin: 6rem auto 3rem;
+            width: 80%;
+            text-align: center;
+          }
+          `}
+      </style>
+
+      <Link className="anchor" to="/">Home</Link>
+    <Link activeClassName="anchor" to="/events">Events</Link>
+      <Link className="anchor" to="/events">Community</Link>
       <a href="#">Programs & Events</a>
       <a href="#">About Us</a>
       <a href="#">Shop</a>
